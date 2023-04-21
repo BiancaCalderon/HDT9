adf817c4847497fddba549c62315f61422860bed
Bianca Calderón <98110978+BiancaCalderon@users.noreply.github.com>
2023-04-20 20:07:15 -0600

    class RBT tree implemented

---
+++ src/ReadFile.java
@@ -0,0 +1,24 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ReadFile {
+    private String filePath;
+
+    public ReadFile(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public List<String> readLines() throws FileNotFoundException {
+        List<String> lines = new ArrayList<>();
+        Scanner scanner = new Scanner(new File(filePath));
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            lines.add(line);
+        }
+        scanner.close();
+        return lines;
+    }
+}
